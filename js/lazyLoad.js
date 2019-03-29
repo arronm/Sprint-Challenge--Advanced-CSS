e0149b23716f087f8487cfc003f2ca99e3c234e4
Arron M <marshall.arron@gmail.com>
2019-03-29 09:37:12 -0700

    Stretch: Add lazy loading

---
+++ js/lazyLoad.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
+ if ("IntersectionObserver" in window) {
+   let lazyObserver = new IntersectionObserver((entries, observer) => {
+     entries.forEach((entry) => {
+       if (entry.isIntersecting) {
+         let lazyImage = entry.target;
+         lazyImage.src = lazyImage.dataset.src;
+         lazyImage.classList.remove('lazy');
+         lazyObserver.unobserve(lazyImage);
+       }
+     })
+   });
+   
+   lazyImages.forEach(lazyImage => lazyObserver.observe(lazyImage));
+ } else {
+   lazyImages.forEach((lazyImage) => {
+     lazyImage.src = lazyImage.dataset.src;
+     lazyImage.classList.remove('lazy');
+   });
+ }
+});
