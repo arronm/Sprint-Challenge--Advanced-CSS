3088eb688bf1e8936020a0bf3eaab949288e9208
Arron M <marshall.arron@gmail.com>
2019-03-29 09:41:54 -0700

    Cleanup JS formatting

--- js/lazyLoad.js
+++ js/lazyLoad.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
- if ("IntersectionObserver" in window) {
+
+  if ("IntersectionObserver" in window) {
    let lazyObserver = new IntersectionObserver((entries, observer) => {
      entries.forEach((entry) => {
        if (entry.isIntersecting) {
@@ -8,15 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
          lazyImage.src = lazyImage.dataset.src;
          lazyImage.classList.remove('lazy');
          lazyObserver.unobserve(lazyImage);
-       }
-     })
-   });
-   
-   lazyImages.forEach(lazyImage => lazyObserver.observe(lazyImage));
- } else {
-   lazyImages.forEach((lazyImage) => {
-     lazyImage.src = lazyImage.dataset.src;
-     lazyImage.classList.remove('lazy');
-   });
- }
+      }
+    })
+  });
+
+    lazyImages.forEach(lazyImage => lazyObserver.observe(lazyImage));
+  } else {
+    lazyImages.forEach((lazyImage) => {
+      lazyImage.src = lazyImage.dataset.src;
+      lazyImage.classList.remove('lazy');
+    });
+  }
 });
